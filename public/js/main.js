1438c157bfbfa11a6bc6047112c632e147bb09c8
tal1992 <timothylam92@hotmail.co.uk>
2021-06-11 18:18:32 +0100

    Coping button functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,16 @@
 import * as store from './store.js';
 import * as wss from './wss.js';
 
-const socket = io('/');
+
+// initialization of socketIO connection
+const socket = io("/");
 wss.registerSocketEvents(socket);
+
+//register event listener for personal code copy button
+const personalCodeCopyButton = document.getElementById(
+  "personal_code_copy_button"
+);
+personalCodeCopyButton.addEventListener("click", () => {
+  const personalCode = store.getState().socketId;
+  navigator.clipboard && navigator.clipboard.writeText(personalCode);
+});
