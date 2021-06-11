1fabe4b89e6fc5516a0e5127e80622f30d73a0ed
tal1992 <timothylam92@hotmail.co.uk>
2021-06-11 18:53:30 +0100

    Code preperation to send pre-offer

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 import * as store from './store.js';
 import * as wss from './wss.js';
+import * as webRTCHandler from './webRTCHandler.js';
+import * as constants from './constants.js';
 
 
 // initialization of socketIO connection
@@ -10,7 +12,40 @@ wss.registerSocketEvents(socket);
 const personalCodeCopyButton = document.getElementById(
   "personal_code_copy_button"
 );
+
 personalCodeCopyButton.addEventListener("click", () => {
   const personalCode = store.getState().socketId;
   navigator.clipboard && navigator.clipboard.writeText(personalCode);
+});
+
+// register event listener for connection buttons
+const personalCodeChatButton = document.getElementById(
+    "personal_code_chat_button"
+);
+
+const personalCodeVideoButton = document.getElementById(
+    "personal_code_video_button"
+);
+
+personalCodeChatButton.addEventListener("click", () => {
+    console.log("chat button clicked");
+
+    const calleePersonalCode = document.getElementById(
+        'personal_code_input'
+    ).value;
+    const callType = constants.callType.CHAT_PERSONAL_CODE;
+
+    webRTCHandler.sendPreOffer(callType, calleePersonalCode);
+    
+});
+
+personalCodeVideoButton.addEventListener("click", () => {
+    console.log("video button clicked");
+    
+    const calleePersonalCode = document.getElementById(
+        'personal_code_input'
+    ).value;
+    const callType = constants.callType.VIDEO_PERSONAL_CODE
+
+    webRTCHandler.sendPreOffer(callType, calleePersonalCode);
 });
---
+++ public/js/webRTCHandler.js
@@ -0,0 +1,5 @@
+export const sendPreOffer = (callType, calleePersonalCode) => {
+    console.log('pre offer function executed');
+    console.log(callType);
+    console.log(calleePersonalCode);
+};
